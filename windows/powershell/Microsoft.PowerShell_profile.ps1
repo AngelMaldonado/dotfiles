c1bb1a0b603edb82047c32c46db7f6982bc6fa88
AngelMaldonado <amaldonadojuarez@gmail.com>
2024-07-25 08:46:26 -0600

    Agregando Aliases de git, cambiando tema de PowerShell, agregando responsively, simplificando UI de vs code y ajuste de atajos (vim + vscode)

--- windows/powershell/Microsoft.PowerShell_profile.ps1
+++ windows/powershell/Microsoft.PowerShell_profile.ps1
@@ -1,2 +1,2 @@
-oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH\montys.omp.json" | Invoke-Expression
+oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH\json.omp.json" | Invoke-Expression
 Import-Module -Name Terminal-Icons
